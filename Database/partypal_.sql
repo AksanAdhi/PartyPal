ba51a4445d58d2dbefac746deb09b08956dade85
AksanAdhi <aksanadhipratama.19@gmail.com>
2024-05-30 01:34:07 +0700

    update

---
+++ Database/partypal_.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2024 at 01:48 AM
+-- Server version: 10.4.32-MariaDB-log
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `partypal.`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('Penyedia','Penyewa') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `username`, `password`, `role`) VALUES
+(9, 'aksan@gmail.com', 'aksan', 'aksan', 'Penyedia'),
+(10, 'nayya@gmail.com', 'Nayya', 'nayya1', 'Penyewa'),
+(11, 'yunnisa@gmail.com', 'Yunnisa', 'yunnisa1', 'Penyedia'),
+(12, 'ghulam@gmail.com', 'Ghulam', 'ghulam1', 'Penyedia');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/koneksi.php
+++ src/koneksi.php
@@ -1,9 +1,9 @@
-<?
+<?php
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$mydb = "partypal";
+$mydb = "partypal.";
 
 $conn = mysqli_connect($servername, $username, $password, $mydb);
 
